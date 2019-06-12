838f87e0891e87b177e1d9372d2ad064b3332067
Thawindu Angesh Tuto <angeshtuto@icloud.com>
2019-06-12 21:42:57 +0530

    Complete Exercise from "Eloquent JavaScript" Chapter 8

---
+++ Eloquent/Chapter8/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Exercise 1 - Retry
+class MultiplicatorUnitFailure extends Error {}
+
+const primitiveMultiply = (x, y) => {
+    const percentage = Math.floor(Math.random() * 100) + 1;
+    if (percentage <= 20) {
+        return x * y;
+    } else {
+        throw new MultiplicatorUnitFailure("Just cause.");
+    }
+};
+
+/**
+ * Keeps calling 'func' until it returns a result.
+ * @param {function} func
+ */
+
+const patchThat = (func, x, y) => {
+    try {
+        console.log(func(x, y));
+    } catch (error) {
+        patchThat(func, x, y);
+    }
+};
+
+patchThat(primitiveMultiply, 2, 5);
